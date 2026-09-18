8ef5c897061f928240bd3f334ae927bdacc92513
hinakof0725 <hinako.f0725@ruri.waseda.jp>
2026-09-18 14:15:24 +0900

    共通ヘッダーと新規登録ページ、ログイン時の遷移先の調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(resource)
+    mypage_path
+  end
 end
